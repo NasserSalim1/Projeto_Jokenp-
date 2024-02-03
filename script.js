d11e60087b5ff3c44db9b8d2c7393fe4f2b1fe03
NasserSalim1 <nasser.salim.beserra@gmail.com>
2024-02-03 14:31:28 -0300

    Adicionando funnções ao site

---
+++ script.js
@@ -0,0 +1,9 @@
+var jogadorNome
+
+document.getElementById('jogador-escolha-1').onclick = function(){};
+document.getElementById('jogador-escolha-2').onclick = function(){};
+document.getElementById('jogador-escolha-3').onclick = function(){};
+
+jogadorNome = prompt("Qual o seu nome?");
+
+document.getElementById('mensagem').innerHTML = jogadorNome
